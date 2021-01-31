d50b642a8aeb28ebe995a0c7aa3e3b0302fe97d1
Yash Soni <73876746+yashsoni294@users.noreply.github.com>
2021-01-31 11:27:14 +0530

    Update Foulty_calculater.py

--- Foulty_calculater.py
+++ Foulty_calculater.py
@@ -1,23 +1,42 @@
-var1=int(input("Enter the 1st No.:"))
-var2=input("Enter operation :")
-var3=int(input("Enter the 2st No.:"))
-if var2=="+":
-    if var1 == 56 and var3 == 9:
-        print(77)
+"""
+Author : Yash Soni
+Date : 31_Jan_2021
+Purpose : Python Learning Purpose
+"""
+while True:
+    try:
+        number_1 = int(input("Enter the first number :\n"))
+        operator = input(
+            "Enter the operation which you want to do on the two numbers :\n")
+        number_2 = int(input("Enter the second number :\n"))
+        break
+    except Exception as e:
+        print(e, "You have entered something wrong !! , Try it again.")
+        continue
+    
+if operator == "+":
+    if number_1 == 56 and number_2 == 9:
+        print("The sum of the two numbers is : ", 77)
+    elif number_2 == 56 and number_1 == 9:
+        print("The sum of the two numbers is : ", 77)
     else:
-        print(var1 + var3)
-elif var2=="-":
-    print(var1-var3)
-elif var2=="*":
-    if var1==45 and var3==3:
-        print(555)
+        print("The sum of the two numbers is : ", number_1+number_2)
+elif operator == "-":
+    print("The substraction of the two numbers is : ", number_1-number_2)
+elif operator == "/":
+    if number_1 == 56 and number_2 == 6:
+        print("The sum of the two numbers is : ", 4)
+    elif number_2 == 56 and number_1 == 6:
+        print("The sum of the two numbers is : ", 4)
     else:
-        print(var1*var3)
-
-elif var2=="/":
-    if var1 == 56 and var3 == 6:
-        print(4)
+        print("The division of the  of the two numbers is : ", number_1/number_2)
+elif operator == "*":
+    if number_1 == 45 and number_2 == 3:
+        print("The sum of the two numbers is : ", 555)
+    elif number_2 == 45 and number_1 == 3:
+        print("The sum of the two numbers is : ", 555)
     else:
-        print(var1 /var3)
+        print("The multiplication of the  of the two numbers is : ", number_1*number_2)
 else:
-    print("Something went wrong. Try again!")
+    print("You have entered wrong operation !!")
+
